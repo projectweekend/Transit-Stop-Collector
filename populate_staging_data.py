14836614415efdc084239eb2ba624d49c4c15ce9
Brian Hines <brian@projectweekend.net>
2015-01-25 15:38:29 -0600

    Refactor to use utils

--- populate_staging_data.py
+++ populate_staging_data.py
@@ -1,29 +1,7 @@
 import os
 import sys
-import psycopg2
-from urlparse import urlparse
 
-
-DATABASE_URL = os.getenv('DATABASE_URL', None)
-assert(DATABASE_URL)
-
-result = urlparse(DATABASE_URL)
-
-connection = psycopg2.connect(
-	database=result.path[1:],
-	user=result.username,
-	password=result.password,
-	host=result.hostname
-)
-
-def connect_to_database():
-	connection = psycopg2.connect(
-		database=result.path[1:],
-		user=result.username,
-		password=result.password,
-		host=result.hostname
-	)
-	return connection, connection.cursor()
+from utils.psql_utils import connect_to_psql
 
 
 def import_query(template, csv_path, file_prefix):
@@ -41,7 +19,7 @@ def main():
 	gtfs_dir = '{0}/gtfs/out'.format(os.getcwd())
 	query = import_query('./tpl/import_from_csv.tpl', gtfs_dir, file_prefix)
 
-	conn, cursor = connect_to_database()
+	conn, cursor = connect_to_psql()
 	cursor.execute(query)
 	conn.commit()
 
