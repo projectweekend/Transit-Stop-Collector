bdb75ab4bee03a12ea79455a5aee7e7a8cbb15df
Brian Hines <brian@projectweekend.net>
2015-01-25 15:02:40 -0600

    Lowercasing some fields

--- sql/chicago_cta/query_routes_with_directions.sql
+++ sql/chicago_cta/query_routes_with_directions.sql
@@ -2,7 +2,7 @@ SELECT          'chicago-cta' as system,
                 r.route_id as id,
                 r.route_long_name as name,
                 'train' as type,
-                'N/A' as directions
+                'n/a' as directions
 FROM            chicago_cta_routes as r
 JOIN            chicago_cta_trips as t
                 ON r.route_id = t.route_id
@@ -15,7 +15,7 @@ SELECT          'chicago-cta' as system,
                 r.route_id as id,
                 r.route_long_name as name,
                 'bus' as type,
-                string_agg(DISTINCT t.direction, ', ') as directions
+                string_agg(DISTINCT lower(t.direction), ', ') as directions
 FROM            chicago_cta_routes as r
 JOIN            chicago_cta_trips as t
                 ON r.route_id = t.route_id
