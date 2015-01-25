2ae37698a5965d119296d57d2c1203074ebf4553
Brian Hines <brian@projectweekend.net>
2015-01-25 14:29:18 -0600

    Rename file

---
+++ sql/chicago_cta/query_routes_with_directions.sql
@@ -0,0 +1,24 @@
+SELECT          r.route_id as id,
+                r.route_long_name as name,
+                'train' as type,
+                'N/A' as directions
+FROM            chicago_cta_routes as r
+JOIN            chicago_cta_trips as t
+                ON r.route_id = t.route_id
+WHERE           r.route_type = 1
+GROUP BY        r.route_id
+
+UNION
+
+SELECT          r.route_id as id,
+                r.route_long_name as name,
+                'bus' as type,
+                string_agg(DISTINCT t.direction, ', ') as directions
+FROM            chicago_cta_routes as r
+JOIN            chicago_cta_trips as t
+                ON r.route_id = t.route_id
+WHERE           r.route_type = 3
+GROUP BY        r.route_id
+ORDER BY        type,
+                id,
+                directions
