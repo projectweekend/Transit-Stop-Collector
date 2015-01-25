ca2ca1b04e9861880a3cbfffcecfa8a1a79fbf84
Brian Hines <brian@projectweekend.net>
2015-01-25 16:00:09 -0600

    A little trimmy, trim, trim

--- sql/chicago_cta_routes.sql
+++ sql/chicago_cta_routes.sql
@@ -1,6 +1,6 @@
 SELECT          'chicago-cta' as system,
-                r.route_id as id,
-                r.route_long_name as name,
+                trim(r.route_id) as id,
+                trim(r.route_long_name) as name,
                 'train' as type,
                 'n/a' as directions
 FROM            chicago_cta_routes as r
@@ -12,10 +12,10 @@ GROUP BY        r.route_id
 UNION
 
 SELECT          'chicago-cta' as system,
-                r.route_id as id,
-                r.route_long_name as name,
+                trim(r.route_id) as id,
+                trim(r.route_long_name) as name,
                 'bus' as type,
-                string_agg(DISTINCT lower(t.direction), ', ') as directions
+                string_agg(DISTINCT lower(trim(t.direction)), ', ') as directions
 FROM            chicago_cta_routes as r
 JOIN            chicago_cta_trips as t
                 ON r.route_id = t.route_id
