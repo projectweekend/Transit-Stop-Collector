689afa325dda0fef6889d0e1263183921fecf230
Brian Hines <brian@projectweekend.net>
2015-01-27 05:52:15 -0600

    Make config a global

--- prepare_gtfs_files.py
+++ prepare_gtfs_files.py
@@ -3,6 +3,18 @@ import csv
 import yaml
 
 
+try:
+	CONFIG_FILE = './config/{0}.yml'.format(sys.argv[1:][0])
+except IndexError:
+	sys.exit("Job name is a required argument. Example: chicago_cta")
+
+try:
+	with open(CONFIG_FILE, 'r') as file:
+		CONFIG = yaml.safe_load(file)
+except IOError:
+	sys.exit("Missing config file for job: '{0}'".format(CONFIG_FILE))
+
+
 def strip_columns(row, columns_to_keep):
 	for k in row.keys():
 		if k not in columns_to_keep:
@@ -25,42 +37,30 @@ def process_gtfs_file(input_file, output_file, columns_to_keep):
 
 def main():
 	try:
-		config_file = './config/{0}.yml'.format(sys.argv[1:][0])
-	except IndexError:
-		sys.exit("Job name is a required argument. Example: chicago_cta")
-
-	try:
-		with open(config_file, 'r') as file:
-			config = yaml.safe_load(file)
-	except IOError:
-		sys.exit("Missing config file for job: '{0}'".format(config_file))
-
-
-	try:
 		to_process = (
 			(
-				'./gtfs/in/{0}'.format(config['routes']['file']),
-				'./gtfs/out/{0}'.format(config['routes']['file']),
-				config['routes']['columns'],
+				'./gtfs/in/{0}'.format(CONFIG['routes']['file']),
+				'./gtfs/out/{0}'.format(CONFIG['routes']['file']),
+				CONFIG['routes']['columns'],
 			),
 			(
-				'./gtfs/in/{0}'.format(config['stops']['file']),
-				'./gtfs/out/{0}'.format(config['stops']['file']),
-				config['stops']['columns'],
+				'./gtfs/in/{0}'.format(CONFIG['stops']['file']),
+				'./gtfs/out/{0}'.format(CONFIG['stops']['file']),
+				CONFIG['stops']['columns'],
 			),
 			(
-				'./gtfs/in/{0}'.format(config['trips']['file']),
-				'./gtfs/out/{0}'.format(config['trips']['file']),
-				config['trips']['columns'],
+				'./gtfs/in/{0}'.format(CONFIG['trips']['file']),
+				'./gtfs/out/{0}'.format(CONFIG['trips']['file']),
+				CONFIG['trips']['columns'],
 			),
 			(
-				'./gtfs/in/{0}'.format(config['stop_times']['file']),
-				'./gtfs/out/{0}'.format(config['stop_times']['file']),
-				config['stop_times']['columns'],
+				'./gtfs/in/{0}'.format(CONFIG['stop_times']['file']),
+				'./gtfs/out/{0}'.format(CONFIG['stop_times']['file']),
+				CONFIG['stop_times']['columns'],
 			),
 		)
 	except KeyError:
-		sys.exit("Config file '{0}' is not formatted properly".format(config_file))
+		sys.exit("Config file '{0}' is not formatted properly".format(CONFIG_FILE))
 
 	for item in to_process:
 		process_gtfs_file(item[0], item[1], item[2])
