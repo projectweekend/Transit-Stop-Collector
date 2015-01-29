60ad18eb74825ffa07e87b4492f2171b9110ae74
Brian Hines <brian@projectweekend.net>
2015-01-29 07:44:16 -0600

    Improve config util

--- utils/config_utils.py
+++ utils/config_utils.py
@@ -2,7 +2,7 @@ import sys
 import yaml
 
 
-def main_config():
+def main_config(args):
 	config_file = './config/.main.yml'
 
 	try:
@@ -11,12 +11,17 @@ def main_config():
 	except IOError:
 		sys.exit("Missing main config file: '{0}'".format(config_file))
 
-	return config, config_file
+	try:
+		return config[args[0]], config_file
+	except IndexError:
+		sys.exit("Target system parameter is required: 'dev' or 'prod'")
+	except KeyError:
+		sys.exit("Target system parameter is not valid: 'dev' or 'prod'")
 
 
 def job_config(args):
 	try:
-		config_file = './config/{0}.yml'.format(args[0])
+		config_file = './config/{0}.yml'.format(args[1])
 	except IndexError:
 		sys.exit("Job name is a required argument. Example: chicago_cta")
 
